b5d1d6d6480a4c72be52a1b069f2a997b83a7d0a
dragonforce2010 <coding.monkey.github@gmail.com>
2023-05-02 14:49:16 +0800

    fixed some issues

--- amplify/backend/function/parseWebsiteTitle/src/index.mjs
+++ amplify/backend/function/parseWebsiteTitle/src/index.mjs
@@ -3,13 +3,20 @@
  */
 
 import cheerio from 'cheerio';
+import fetch from 'node-fetch';
 
-exports.handler = async (event) => {
+export const handler = async (event) => {
   console.log(`EVENT: ${JSON.stringify(event)}`);
-  const url = event.url;
+  console.log(`event body`, event.body);
+  const payload = event.body;
+  const url = payload.url;
   if (!url) {
     return {
       statusCode: 400,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Content-Type': 'application/json',
+      },
       error: `bad request! url is not valid, url = ${url}`,
     };
   }
@@ -26,12 +33,20 @@ exports.handler = async (event) => {
 
     return {
       statusCode: 200,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Content-Type': 'application/json',
+      },
       websiteTitle: websiteTitle,
     };
   } catch (err) {
     console.log(err);
     return {
       statusCode: 500,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Content-Type': 'application/json',
+      },
       error: `internal error`,
     };
   }
